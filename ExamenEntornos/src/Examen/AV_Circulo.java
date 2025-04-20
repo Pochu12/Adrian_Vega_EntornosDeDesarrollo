c74070eb370250d18314c27e756e8d0c194b623f
adri vega <charly2adrii@gmail.com>
2025-04-20 12:56:19 +0200

    AV_Commit de la segunda clase cuadrado

--- ExamenEntornos/src/Examen/AV_Circulo.java
+++ ExamenEntornos/src/Examen/AV_Circulo.java
@@ -1,5 +1,57 @@
 package Examen;
 
 public class AV_Circulo {
+	private int id;
+	private double radio;
+	private String color;
+	
+	
+	public AV_Circulo() {}
+	
+	public AV_Circulo(int id, double radio, String color) {
+		super();
+		this.id = id;
+		this.radio = radio;
+		this.color = color;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getRadio() {
+		return radio;
+	}
+
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "AV_Circulo [id=" + id + ", radio=" + radio + ", color=" + color + "]";
+	}
+
+
+	
+    
+    
+	
+	
+	
+	
+	
+	
 }
